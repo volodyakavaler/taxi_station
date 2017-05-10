03bf3cae6d744bf58896a362f1d039bb20f4dd1a
vladimir krysanov <vkrysanov320@gmail.com>
2017-05-10 14:58:09 +0300

    Update search

--- app/models/order.rb
+++ app/models/order.rb
@@ -20,8 +20,9 @@ class Order < ActiveRecord::Base
 
   # поиск
   def self.search(params)
-    result = Order.includes(:automobile, :tariff)
+    result = Order.includes(:tariff, {:automobile => :drivers}).references(:tariff, {:automobile => :drivers})
 
+    # по атрибутам заказа:
     if params['time_of_travel_from'].present?
       result = result.where.not(time_of_travel: Time.at(0)...Time.parse(params['time_of_travel_from']))
     end
@@ -43,11 +44,59 @@ class Order < ActiveRecord::Base
     if params['length_of_route_to'].present?
       result = result.where(length_of_route: 0..(params['length_of_route_to']).to_f)
     end
-    if params['automobile'].present?
-      result = result.where(automobiles: {state_number: params['automobile']})
+
+    # по атрибутам тарифа
+    if params['tariff_name'].present?
+      result = result.where(tariffs: {name: params['tariff_name']})
     end
-    if params['tariff'].present?
-      result = result.where(tariffs: {name: params['tariff']})
+    if params['time_of_day'].present?
+      result = result.where(tariffs: {time_of_day: params['time_of_day']})
+    end
+    if params['range'].present?
+      result = result.where(tariffs: {range: params['range']})
+    end
+    if params['price_per_kilometer_from'].present?
+      result = result.where.not(tariffs: {price_per_kilometer: 0...(params['price_per_kilometer_from']).to_f})
+    end
+    if params['price_per_kilometer_to'].present?
+      result = result.where(tariffs: {price_per_kilometer: 0..(params['price_per_kilometer_to']).to_f})
+    end
+
+    # по атрибутам авто:
+    if params['automobile_model'].present?
+      result = result.where(automobiles: {automobile_model: params['automobile_model']})
+    end
+    if params['automobile_type'].present?
+      result = result.where(automobiles: {automobile_type: params['automobile_type']})
+    end
+    if params['state_number'].present?
+      result = result.where(automobiles: {state_number: params['state_number']})
+    end
+    if params['color'].present?
+      result = result.where(automobiles: {color: params['color']})
+    end
+    if params['release'].present?
+      result = result.where(automobiles: {release: params['release']})
+    end
+
+    # по атрибутам водителя:
+    if params['driver_last_name'].present?
+      result = result.where(drivers: {last_name: params['driver_last_name']})
+    end
+    if params['driver_first_name'].present?
+      result = result.where(drivers: {first_name: params['driver_first_name']})
+    end
+    if params['driver_patronymic'].present?
+      result = result.where(drivers: {patronymic: params['driver_patronymic']})
+    end
+    if params['driver_date_of_birth'].present?
+      result = result.where(drivers: {date_of_birth: params['driver_date_of_birth']})
+    end
+    if params['driver_itn'].present?
+      result = result.where(drivers: {itn: params['driver_itn']})
+    end
+    if params['driver_passport'].present?
+      result = result.where(drivers: {passport: params['driver_passport']})
     end
 
     result.all
