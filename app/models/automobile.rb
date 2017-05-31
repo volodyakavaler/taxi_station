2a38dc006cc9275c6eb12e2f5124dd195be77cc2
vladimir krysanov <vkrysanov320@gmail.com>
2017-06-01 01:19:16 +0300

    update all(2)

--- app/models/automobile.rb
+++ app/models/automobile.rb
@@ -4,7 +4,7 @@ class Automobile < ActiveRecord::Base
 
   validates :automobile_model, length: {minimum: 5, maximum: 30}
   validates :automobile_type, inclusion: {in: ["Бизнес", "Эконом"]}
-  validates :state_number, format: {with: /(а|в|е|к|м|н|о|р|с|т|у|х){2}\d{3}\d{2,3}/}, uniqueness: true
+  validates :state_number, format: {with: /(а|в|е|к|м|н|о|р|с|т|у|х){2}\d{3}\d{2,3}\Z/}, uniqueness: true
   validates :color, presence: true, length: {maximum: 20}
   validates :release, numericality: {greater_than_or_equal_to: 1900, less_than_or_equal_to: Date.today.year}
 end
